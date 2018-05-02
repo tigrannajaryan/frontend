2da8732081c27ae48d664f256df7c32bfe1dd917
dmitriy <di3orlive@yahoo.com>
2018-05-02 16:19:04 +0300

    start working on FB login plugin & packege added & login function

--- mobile/src/pages/first-screen/first-screen.ts
+++ mobile/src/pages/first-screen/first-screen.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook';
+
 import { PageNames } from '../page-names';
 
 /**
@@ -16,7 +18,11 @@ import { PageNames } from '../page-names';
 })
 export class FirstScreenComponent {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private fb: Facebook
+  ) {
   }
 
   loginByEmail(): void {
@@ -26,5 +32,25 @@ export class FirstScreenComponent {
   register(): void {
     // this.navCtrl.push(PageNames.Services, {}, {animate: false});
     this.navCtrl.push(PageNames.RegisterByEmail, {}, {animate: false});
+  }
+
+  loginByFb(): void {
+    this.fb.login(['public_profile', 'user_friends', 'email'])
+      .then(res => {
+        if (res.status === 'connected') {
+          this.getUserDetail(res.authResponse.userID);
+        }
+      })
+      .catch(e => console.dir('Error logging into Facebook', e));
+  }
+
+  getUserDetail(userid): void {
+    this.fb.api(`/${userid}/?fields=id`, ['public_profile'])
+      .then(res => {
+        console.dir(res);
+      })
+      .catch(e => {
+        console.dir(e);
+      });
   }
 }
