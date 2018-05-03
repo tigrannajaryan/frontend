41ad06fbd6796a49ce5f2db8d5a1ae252c50eaaa
dmitriy <di3orlive@yahoo.com>
2018-05-03 21:10:33 +0300

    trying to connect

--- mobile/src/environments/environment.prod.ts
+++ mobile/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const ENV = {
-  apiUrl: 'http://admindev.betterbeauty.io/api/v1/',
+  apiUrl: 'http://192.168.31.170:8000/api/v1/',  // for testing fb login via my ip
+  // apiUrl: 'http://admindev.betterbeauty.io/api/v1/',
   production: true,
   version: '0.0.1'
 };
