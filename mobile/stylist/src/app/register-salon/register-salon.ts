8088d3c229957f92bd4cfc1604953c90e93cff5b
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2018-07-01 02:29:22 +0300

    Show alert with backend error msg
    Github issue: #366

--- mobile/stylist/src/app/register-salon/register-salon.ts
+++ mobile/stylist/src/app/register-salon/register-salon.ts
@@ -181,9 +181,16 @@ export class RegisterSalonComponent {
       // tslint:disable-next-line:no-null-keyword
       salon_name: profile.salon_name || null
     };
-    await this.apiService.setProfile(data);
-
-    this.nextRoute();
+    try {
+      await this.apiService.setProfile(data);
+      this.nextRoute();
+    } catch (e) {
+      if (e.errors instanceof Map && e.errors.size > 0) {
+        showAlert('Saving your info failed', Array.from(e.errors.values())[0][0].code);
+      } else {
+        throw e; // re-throw
+      }
+    }
   }
 
   processPhoto(): void {
