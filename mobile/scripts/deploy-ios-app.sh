0a4577a6adbe2ec197e19f079cb5c092e3e9de88
Alexei Mironov <alexeidjango@gmail.com>
2018-07-04 15:56:29 +0300

    [HOTFIX] Fix deploy groups param without ext. distribution

--- mobile/scripts/deploy-ios-app.sh
+++ mobile/scripts/deploy-ios-app.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 echo "Deploying to TestFlight"
-fastlane pilot upload -u $FASTLANE_USER -i "ios_build/$IOS_SDK/$IOS_APP_NAME-export/$IOS_APP_NAME.ipa" -a $IOS_APP_BUNDLE_ID --changelog "$TRAVIS_COMMIT_MESSAGE" --skip_waiting_for_build_processing $FASTLANE_SKIP_WAITING --distribute_external $FASTLANE_DISTRIBUTE_EXTERNAL --groups "$FASTLANE_EXTERNAL_GROUP_LIST"
+fastlane pilot upload -u $FASTLANE_USER -i "ios_build/$IOS_SDK/$IOS_APP_NAME-export/$IOS_APP_NAME.ipa" -a $IOS_APP_BUNDLE_ID --changelog "$TRAVIS_COMMIT_MESSAGE" --skip_waiting_for_build_processing $FASTLANE_SKIP_WAITING --distribute_external $FASTLANE_DISTRIBUTE_EXTERNAL
