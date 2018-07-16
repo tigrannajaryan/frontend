38d137bbc5d3140173ffc2f8c75edf5182f06adb
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2018-07-16 19:46:57 +0300

    Add dummy spec
    Github issue: #412

---
+++ mobile/client/src/app/auth-page/auth-page.component.spec.ts
@@ -0,0 +1,8 @@
+describe('Pages: Auth', () => {
+  // TODO: tests should be implemented
+  it('should create the page', () => {
+    // This line of code prevents Karma from raising
+    // an error because of empty tests:
+    expect(true).toBeTruthy();
+  });
+});
