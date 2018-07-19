24cb27e2ece03ca9477ed3f8c6188b48091c1b09
Diego Estela <daleposc@gmail.com>
2018-07-19 16:43:10 -0300

    Documentation update.

--- mobile/stylist/src/app/core/events/event-types.ts
+++ mobile/stylist/src/app/core/events/event-types.ts
@@ -1,6 +1,9 @@
 export enum EventTypes {
   /**
    * Event fired when StylistServiceProvider.getProfile() gets resolved.
+   * This event is needed because the Google Maps api loads the api key dynamically.
+   * When this event is fired, the subscriber in GoogleMapsConfig updates the api key
+   * and the google library automatically updates it's key.
    */
   UPDATE_GMAP_KEY = 'UPDATE_GMAP_KEY'
 
