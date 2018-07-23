cdae48a42853856ca0e5c889c8d9fc663c6daae4
Diego Estela <daleposc@gmail.com>
2018-07-23 16:52:08 -0300

    Compilation Issue.

--- mobile/client/src/app/core/effects/profile.effects.ts
+++ mobile/client/src/app/core/effects/profile.effects.ts
@@ -1,22 +1,16 @@
-import {Injectable} from '@angular/core';
-import {Actions, Effect, ofType} from '@ngrx/effects';
+import { Injectable } from '@angular/core';
+import { Actions, Effect } from '@ngrx/effects';
 import {
   profileActionTypes,
   RequestUpdateProfileAction,
   RequestUpdateProfileSucceedAction
 } from '~/core/reducers/profile.reducer';
-import {ProfileServiceMock} from '~/core/api/profile-service.mock';
-import {map} from 'rxjs/operators';
-import {Observable} from "rxjs/Observable";
-import {Action} from '@ngrx/store';
-import {mergeMap} from "rxjs/operator/mergeMap";
+import { ProfileServiceMock } from '~/core/api/profile-service.mock';
+import { Observable } from "rxjs/Observable";
+import { Action } from '@ngrx/store';
 
 @Injectable()
 export class ProfileEffects {
-
-  constructor(protected actions$: Actions, protected profileService: ProfileServiceMock) {
-
-  }
 
   /**
    * Here we implement the updateProfile effect that will cal the API to update a profile.
@@ -24,10 +18,14 @@ export class ProfileEffects {
    */
   @Effect()
   updateProfile: Observable<Action> = this.actions$
-    .pipe(
-      ofType<RequestUpdateProfileAction>(profileActionTypes.REQUEST_UPDATE_PROFILE),
-      mergeMap(action => this.profileService.getProfile(action.profile)),
-      map(profile => new RequestUpdateProfileSucceedAction(profile))
-    );
+    .ofType<RequestUpdateProfileAction>(profileActionTypes.REQUEST_UPDATE_PROFILE)
+    .switchMap(action => {
+      return this.profileService.updateProfile(action.profile)
+    })
+    .map(profile => new RequestUpdateProfileSucceedAction(profile))
+
+    constructor(protected actions$: Actions, protected profileService: ProfileServiceMock) {
+
+    }
 
 }
