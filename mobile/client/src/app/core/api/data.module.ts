0bd6d18f4cfd72981055cba7f5646bdc5d07b323
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2018-08-10 20:08:14 +0300

    Add all services in data module
    Github issue: #508

--- mobile/client/src/app/core/api/data.module.ts
+++ mobile/client/src/app/core/api/data.module.ts
@@ -4,7 +4,17 @@ import { IonicModule } from 'ionic-angular';
 import { AppointmentsHistoryApi } from '~/core/api/appointments-history.api';
 import { AppointmentsHistoryDataStore } from '~/core/api/appointments-history.data';
 
+import { AuthService } from '~/core/api/auth-service';
+import { AuthServiceMock } from '~/core/api/auth-service.mock';
+
+import { ProfileService } from '~/core/api/profile-service';
 import { ProfileDataStore } from '~/profile/profile.data';
+
+import { ServicesService } from '~/core/api/services-service';
+import { ServicesServiceMock } from '~/core/api/services-service.mock';
+
+import { StylistsService } from '~/core/api/stylists-service';
+import { StylistsServiceMock } from '~/core/api/stylists-service.mock';
 
 /**
  * Common data module that includes ApiDataStore singletons for the entire app.
@@ -16,7 +26,17 @@ import { ProfileDataStore } from '~/profile/profile.data';
   ],
   // Add API service providers in the 'providers' array here
   providers: [
-    AppointmentsHistoryApi
+    // services
+    AppointmentsHistoryApi,
+    AuthService,
+    ProfileService,
+    StylistsService,
+    ServicesService,
+
+    // services mocks
+    AuthServiceMock,
+    StylistsServiceMock,
+    ServicesServiceMock
   ]
 })
 export class DataModule {
