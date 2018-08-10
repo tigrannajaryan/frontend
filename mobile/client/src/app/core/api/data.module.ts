f6f34576d74269d4777c9317535f1753e5798936
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2018-08-10 20:22:27 +0300

    Remove mocks
    Github issue: #508

--- mobile/client/src/app/core/api/data.module.ts
+++ mobile/client/src/app/core/api/data.module.ts
@@ -2,19 +2,13 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 
 import { AppointmentsHistoryApi } from '~/core/api/appointments-history.api';
-import { AppointmentsHistoryDataStore } from '~/core/api/appointments-history.data';
-
 import { AuthService } from '~/core/api/auth-service';
-import { AuthServiceMock } from '~/core/api/auth-service.mock';
-
 import { ProfileService } from '~/core/api/profile-service';
-import { ProfileDataStore } from '~/profile/profile.data';
-
 import { ServicesService } from '~/core/api/services-service';
-import { ServicesServiceMock } from '~/core/api/services-service.mock';
-
 import { StylistsService } from '~/core/api/stylists-service';
-import { StylistsServiceMock } from '~/core/api/stylists-service.mock';
+
+import { AppointmentsHistoryDataStore } from '~/core/api/appointments-history.data';
+import { ProfileDataStore } from '~/profile/profile.data';
 
 /**
  * Common data module that includes ApiDataStore singletons for the entire app.
@@ -26,17 +20,11 @@ import { StylistsServiceMock } from '~/core/api/stylists-service.mock';
   ],
   // Add API service providers in the 'providers' array here
   providers: [
-    // services
     AppointmentsHistoryApi,
     AuthService,
     ProfileService,
     StylistsService,
-    ServicesService,
-
-    // services mocks
-    AuthServiceMock,
-    StylistsServiceMock,
-    ServicesServiceMock
+    ServicesService
   ]
 })
 export class DataModule {
