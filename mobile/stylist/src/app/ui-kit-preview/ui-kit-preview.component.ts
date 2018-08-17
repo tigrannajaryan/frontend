cb33f14f0767be6c765d389d3ca5f66996fcec03
dmitriy <di3orlive@yahoo.com>
2018-08-17 17:27:04 +0300

    Add made-beauty icons to stylist and client app (#611)
    Added font icons to client and stylist app
    Added ui page (http://localhost:8100/#/ui) for common components etc.
    here we can keep components that related to current app (and easily check what you need here, as I did with mb-icons)

---
+++ mobile/stylist/src/app/ui-kit-preview/ui-kit-preview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage({
+  segment: 'ui'
+})
+@Component({
+  selector: 'page-ui-kit-preview',
+  templateUrl: 'ui-kit-preview.component.html'
+})
+export class UiKitPreviewComponent {
+}
