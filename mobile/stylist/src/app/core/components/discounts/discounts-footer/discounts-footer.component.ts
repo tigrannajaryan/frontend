0ff9bb21848ed8295ed79afaa52b7453a0ed6b20
Anahit Sharoyan <anahit.sharoyan@gmail.com>
2018-08-20 11:55:24 -0400

    Fix footer invite client button according to comments. Fix header back arrow.

--- mobile/stylist/src/app/core/components/discounts/discounts-footer/discounts-footer.component.ts
+++ mobile/stylist/src/app/core/components/discounts/discounts-footer/discounts-footer.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class DiscountsFooterComponent {
   @Input() disabled: boolean;
   @Output() continue = new EventEmitter();
+  @Input() title: string;
 
   onContinue(): void {
     this.continue.emit();
