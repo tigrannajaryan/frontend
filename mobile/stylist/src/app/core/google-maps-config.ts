ead00e1e0412184fab59dbc0af73bf27b469f034
tigrannajaryan <4194920+tigrannajaryan@users.noreply.github.com>
2018-08-21 12:26:57 -0400

    Fix Google Maps API usage bug (#649)
    Google Maps API was not correctly initialized. There was a race
    condition when we would start initializing Google Maps API
    without waiting for the Google Maps API key to arrive from our
    backend. This is now fixed, we ensure that we wait for it first.
    
    Github: #614

--- mobile/stylist/src/app/core/google-maps-config.ts
+++ mobile/stylist/src/app/core/google-maps-config.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Events } from 'ionic-angular';
 import { LazyMapsAPILoaderConfigLiteral } from '@agm/core';
+
+import { Logger } from '~/shared/logger';
 import { EventTypes } from '~/core/events/event-types';
 
 enum GoogleMapsLibraries {
@@ -13,8 +15,12 @@ export class GoogleMapsConfig implements LazyMapsAPILoaderConfigLiteral {
   apiKey: string;
   readonly libraries = [GoogleMapsLibraries.PLACES];
 
-  constructor(private events: Events) {
+  constructor(
+    private events: Events,
+    private logger: Logger
+  ) {
     this.events.subscribe(EventTypes.UPDATE_GMAP_KEY, apiKey => {
+      this.logger.info('GoogleMapsConfig received api key=', apiKey);
       this.apiKey = apiKey;
       this.events.unsubscribe(EventTypes.UPDATE_GMAP_KEY);
     });
