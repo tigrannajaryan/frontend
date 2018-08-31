eeef04c19b8ae4c001b1110360dc551b76a0bdac
dmitriy <di3orlive@yahoo.com>
2018-08-31 16:44:10 +0300

    Make client app header according to design (#727)
    - Added hideBackButton input field to header
    - Added showHome input field to header
    - Change wrappers for pages with new header
    - Add Home button (only during booking process)
    
    Issue: #700

--- mobile/client/src/app/core/components/made-header/made-header.component.ts
+++ mobile/client/src/app/core/components/made-header/made-header.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { PageNames } from '~/core/page-names';
 
 @Component({
   selector: '[madeHeader]',
   templateUrl: 'made-header.component.html'
 })
 export class MadeHeaderComponent {
+  @Input() showHome: boolean;
+  @Input() hideBackButton: boolean;
+
+  constructor(private navCtrl: NavController) {}
+
+  goHome(): void {
+    this.navCtrl.setRoot(PageNames.MainTabs);
+  }
 }
