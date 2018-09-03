92858358f0b1146cb51bf715537247e3dbf2e7c0
Alexei Mironov <alexeidjango@gmail.com>
2018-09-03 22:43:34 +0300

    [HOTFIX] Manually generate sentry.properties for dsym upload

--- mobile/scripts/travis_install.sh
+++ mobile/scripts/travis_install.sh
@@ -5,3 +5,13 @@ set -ev
 echo "Running INSTALL phase"
 cd $TRAVIS_BUILD_DIR/mobile
 make -j 8 build-$APP_TYPE LOG=yes
+
+# temporary hotfix - generate sentry properties file
+
+cat > $TRAVIS_BUILD_DIR/mobile/$APP_TYPE/sentry.properties << EOL
+defaults.url=https://sentry.io/
+defaults.org=$SENTRY_ORGANIZATION
+defaults.project=$SENTRY_PROJECT
+auth.token=$SENTRY_AUTH_TOKEN
+cli.executable=node_modules/@sentry/cli/bin/sentry-cli
+EOL
