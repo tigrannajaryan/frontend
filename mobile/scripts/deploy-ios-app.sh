5a2a4672fb8f1b745f072f55257fd08f2df666c6
Alexei Mironov <alexeidjango@gmail.com>
2018-09-05 19:50:33 +0300

    [HOTFIX] Force-update Fastlane version to fix iOS uploads

--- mobile/scripts/deploy-ios-app.sh
+++ mobile/scripts/deploy-ios-app.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 echo "Deploying to TestFlight"
+
+# we should not fix fastlane version here, since we really do depend on freshness
+# of Fastlane: they follow changes in appStoreConnect
+gem install fastlane -v '>= 2.103.1'
+
 fastlane pilot upload -u $FASTLANE_USER -i "ios_build/$IOS_SDK/$IOS_APP_NAME-export/$IOS_APP_NAME.ipa" -a $IOS_APP_BUNDLE_ID --changelog "$TRAVIS_COMMIT_MESSAGE" --skip_waiting_for_build_processing $FASTLANE_SKIP_WAITING --distribute_external $FASTLANE_DISTRIBUTE_EXTERNAL
 # deploy build artifacts to Sentry
 echo "Deploying source maps to Sentry"
