d3c37d4b157112341886033bba5d0d4cb86ed803
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2018-09-24 20:08:16 +0300

    Use unreal numbers

--- mobile/client/src/app/core/utils/test-utils.ts
+++ mobile/client/src/app/core/utils/test-utils.ts
@@ -1,8 +1,8 @@
 /**
- * Generates normalized random NY landline phone
+ * Generates normalized not existed phone number
  */
 export function randomPhone(): string {
-  return `+1347${Math.random().toFixed(9).toString().slice(2, 9)}`;
+  return `+1555${Math.random().toFixed(9).toString().slice(2, 9)}`;
 }
 
 /**
