0f3f523d955dcfeacac56eddc9ecc8768e01b445
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2018-10-04 21:10:23 +0300

    Add helper method to simulate http errors

--- mobile/shared/code/api/base-service.mock.ts
+++ mobile/shared/code/api/base-service.mock.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { ApiResponse } from '~/shared/api/base.models';
 import { BaseService } from '~/shared/api/base-service';
+import { ApiError, HttpStatus } from '~/shared/api-errors';
 
 @Injectable()
 export class BaseServiceMock extends BaseService {
   protected mockRequest<ResponseType>(responseMock: Observable<ResponseType>): Observable<ApiResponse<ResponseType>> {
     return this.prepareResponse('', '', responseMock, {});
+  }
+
+  protected simulateHttpError<ResponseType>(status: HttpStatus, error: ApiError): Observable<ApiResponse<ResponseType>> {
+    return this.prepareResponse('', '',
+      Observable.create(() => {
+        throw new HttpErrorResponse({ status, error });
+      }), {}
+    );
   }
 }
