256b4ad30a323b277b076f4d1e553c473e2771a4
dmitriy <di3orlive@yahoo.com>
2018-10-05 07:16:11 -0400

    Fix missing `new` tag
    We should keep the rules for existing tags: NOW, NEXT, NOT CHECKEDOUT.
    If none of these tags is applicable then check the rule for NEW tag: (booked time >= now-12hours)
    
    Issue: FRON-933

--- mobile/stylist/src/app/home/home.component.ts
+++ mobile/stylist/src/app/home/home.component.ts
@@ -23,7 +23,8 @@ import { LoadProfileAction, ProfileState, selectProfile } from '~/core/component
 export enum AppointmentTag {
   NotCheckedOut = 'Not checked out',
   Now = 'Now',
-  Next = 'Next'
+  Next = 'Next',
+  New = 'New'
 }
 
 export enum Tabs {
@@ -284,6 +285,8 @@ export class HomeComponent {
       let metNext = false;
       for (const appointment of home.appointments) {
         const startTime = moment(new Date(appointment.datetime_start_at));
+        const createdAt = moment(new Date(appointment.created_at));
+        const pastTime = moment(new Date()).subtract({hours: 12});
 
         const endTime = startTime.clone();
         endTime.add(appointment.duration_minutes, 'minutes');
@@ -297,11 +300,11 @@ export class HomeComponent {
           } else {
             tag = AppointmentTag.NotCheckedOut;
           }
-        } else {
-          if (!metNext) {
-            tag = AppointmentTag.Next;
-            metNext = true;
-          }
+        } else if (!metNext) {
+          tag = AppointmentTag.Next;
+          metNext = true;
+        } else if (createdAt >= pastTime) {
+          tag = AppointmentTag.New;
         }
         appointmentTags.push(tag);
       }
