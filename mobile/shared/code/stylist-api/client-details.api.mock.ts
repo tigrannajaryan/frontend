e13ecb2046a6127f5a35600842d5e803b13087ce
Vlad Shulyugin <vladislav.shulyugin@gmail.com>
2018-10-05 19:29:11 +0300

    Implement Client screen in Stylist App (#921)
    Issue: FRON-932

---
+++ mobile/shared/code/stylist-api/client-details.api.mock.ts
@@ -0,0 +1,32 @@
+import * as faker from 'faker';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { ApiResponse } from '~/shared/api/base.models';
+import { BaseServiceMock } from '~/shared/api/base-service.mock';
+import { ClientDetailsModel } from './client-details.models';
+
+export const clientDetailsMock: ClientDetailsModel = {
+    uuid: faker.random.uuid(),
+    first_name: faker.name.firstName(),
+    last_name: faker.name.lastName(),
+    phone: faker.phone.phoneNumber(),
+    city: faker.address.city(),
+    state: faker.address.state(),
+    photo: faker.image.imageUrl(),
+    email: faker.internet.email(),
+    last_visit_datetime: new Date().toISOString(),
+    last_services_names: [faker.lorem.word(), faker.lorem.word()]
+};
+
+@Injectable()
+export class ClientDetailsApiMock extends BaseServiceMock {
+    getClientDetails(): Observable<ApiResponse<ClientDetailsModel>> {
+        return this.mockRequest<ClientDetailsModel>(
+            Observable.create(observer => {
+                observer.next(clientDetailsMock);
+                observer.complete();
+            })
+        );
+    }
+}
