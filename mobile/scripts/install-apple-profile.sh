66250e7391c0e06ea4760ca51c8e03e7dcaed329
Alexei <alexeidjango@users.noreply.github.com>
2018-10-30 00:41:45 +0300

    Replace prov. profiles with push-enabled, and separate stylists prov. profile (#1014)
    Replaces provisioning profiles used for building iOS apps with separate
    profile per application, non-wildcard profiles.

--- mobile/scripts/install-apple-profile.sh
+++ mobile/scripts/install-apple-profile.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-set -e
-echo "Installing provisioning profile"
+
+set -ev
+echo "Installing provisioning profiles"
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp mobile/ios-cert/*.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
