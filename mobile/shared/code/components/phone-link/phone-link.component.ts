3e3be5934c767b970163d182f5a659f09072233b
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2018-11-11 18:34:20 +0300

    Allow to copy or call phone

--- mobile/shared/code/components/phone-link/phone-link.component.ts
+++ mobile/shared/code/components/phone-link/phone-link.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActionSheetController } from 'ionic-angular';
+import { ActionSheetButton } from 'ionic-angular/components/action-sheet/action-sheet-options';
 
 import { ExternalAppService } from '~/shared/utils/external-app-service';
 import { getPhoneNumber } from '~/shared/utils/phone-numbers';
@@ -8,21 +10,46 @@ import { NumberFormat } from '~/shared/directives/phone-input.directive';
   selector: 'phone-link',
   templateUrl: 'phone-link.component.html'
 })
-export class PhoneLinkComponent {
+export class PhoneLinkComponent implements OnInit {
   @Input() phone: string;
   @Input() readonly = false;
   @Input() shortForm = true;
   @Input() iconForm = false;
   @Input() icon?: string;
 
+  private buttons: ActionSheetButton[];
+
   constructor(
+    private actionSheetCtrl: ActionSheetController,
     private externalAppService: ExternalAppService
   ) {
   }
 
+  ngOnInit(): void {
+    this.buttons = [
+      {
+        text: `Copy ${this.phone}`,
+        handler: () => {
+          this.externalAppService.copyToTheClipboard(this.phone);
+        }
+      },
+      {
+        text: `Dial ${this.phone}`,
+        handler: () => {
+          this.externalAppService.doPhoneCall(this.phone);
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel'
+      }
+    ];
+  }
+
   async onClick(): Promise<void> {
     if (!this.readonly) {
-      this.externalAppService.doPhoneCall(this.phone);
+      const actionSheet = this.actionSheetCtrl.create({ buttons: this.buttons });
+      actionSheet.present();
     }
   }
 
