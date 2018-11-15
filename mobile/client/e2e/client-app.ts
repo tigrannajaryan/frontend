cf70fdcfb1caa4702afdb27af45d44ce5de2cffd
Vlad Shulyugin <vladislav.shulyugin@gmail.com>
2018-11-15 21:13:34 +0300

    Add code to reset browser and its data in e2e tests (#1051)
    - Use only incognito mode
    - Add browser reset

--- mobile/client/e2e/client-app.ts
+++ mobile/client/e2e/client-app.ts
@@ -1,14 +1,16 @@
 import { browser } from 'protractor';
 
-import { clearIonicStorage } from './shared-e2e/utils';
+import { clearIonicStorage, clearSessionData } from './shared-e2e/utils';
 
 /**
  * ClientApp definition
  */
 class ClientApp {
   async loadNew() {
+    await browser.restart();
     await clearIonicStorage();
     await browser.get('');
+    await clearSessionData();
   }
 }
 
