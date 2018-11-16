30cecb5f5b0c429888fdfbb40b1fa94623756abe
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2018-11-16 18:49:43 +0300

    Rollbacke date format

--- mobile/client/src/app/core/api/booking.api.ts
+++ mobile/client/src/app/core/api/booking.api.ts
@@ -46,7 +46,7 @@ export class BookingApi extends BaseService {
 
   getTimeslots(stylistUuid: string, date: moment.Moment): Observable<ApiResponse<TimeslotsResponse>> {
     const params = {
-      date: date.format(isoDateFormat),
+      date: date.format('YYYY-MM-DD'),
       stylist_uuid: stylistUuid
     };
     return this.post<TimeslotsResponse>('client/available-times', params);
