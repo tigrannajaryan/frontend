1eba29fc5b6629cd0c3881d44565f4923e82ecdc
dmitriy <di3orlive@yahoo.com>
2018-11-21 07:21:32 -0500

    Fix unknown version and progile not change problem
    - Move version and build number back to legal page
    - Disable swipe to open menu on some pages
    - Fix profile not change problem
    
    FRON-1139 FRON-1144 FRON-1141

--- mobile/client/src/app/about/about.component.ts
+++ mobile/client/src/app/about/about.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { AppVersion } from '@ionic-native/app-version';
 
-import { getBuildNumber, getCommitHash } from '~/shared/get-build-info';
+import { getAppVersionNumber, getBuildNumber, getCommitHash } from '~/shared/get-build-info';
 import { ENV } from '~/environments/environment.default';
 import Licenses from '~/core/data/licenses.json';
 
@@ -11,27 +10,16 @@ import Licenses from '~/core/data/licenses.json';
   templateUrl: 'about.component.html'
 })
 export class AboutComponent {
+  getAppVersionNumber = getAppVersionNumber;
   getBuildNumber = getBuildNumber;
   commitHash = getCommitHash;
   easterEggCounter = 0;
-  appVersion: string;
   licenses = Licenses;
 
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams,
-    verProvider: AppVersion
+    public navParams: NavParams
   ) {
-    this.init(verProvider);
-  }
-
-  async init(verProvider: AppVersion): Promise<void> {
-    try {
-      this.appVersion = await verProvider.getVersionNumber();
-    } catch (e) {
-      // Most likely running in browser so Cordova is not available. Ignore.
-      this.appVersion = 'Unknown';
-    }
   }
 
   getEnv(): typeof ENV {
