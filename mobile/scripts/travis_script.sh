902510b2c6d10ef016e40be9ddc9f7bc9a93a5b0
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2018-12-10 16:09:44 +0300

    Enable builds in PRs

--- mobile/scripts/travis_script.sh
+++ mobile/scripts/travis_script.sh
@@ -4,10 +4,10 @@ set -evx
 
 # if this is a pull request - just exit, we don't want to build actual
 # apps during PR builds
-if [[ $TRAVIS_PULL_REQUEST != "false" ]]; then
-    echo "Skipping actual mobile apps build on a pull request"
-    exit 0
-fi
+# if [[ $TRAVIS_PULL_REQUEST != "false" ]]; then
+#     echo "Skipping actual mobile apps build on a pull request"
+#     exit 0
+# fi
 
 # install android pre-requisites
 $TRAVIS_BUILD_DIR/mobile/scripts/install-android.sh
@@ -27,15 +27,15 @@ $TRAVIS_BUILD_DIR/mobile/scripts/build-android-app.sh
 
 # prepare and build iOS app
 
-export APP_BUNDLE_ID=$IOS_APP_BUNDLE_ID
+# export APP_BUNDLE_ID=$IOS_APP_BUNDLE_ID
 
-cd $TRAVIS_BUILD_DIR
-# restore encrypted distribution certificate file
-openssl aes-256-cbc -K $encrypted_d801e47d7db5_key -iv $encrypted_d801e47d7db5_iv -in mobile/ios-cert/distribution.p12.enc -out mobile/ios-cert/distribution.p12 -d
-mobile/scripts/install-apple-profile.sh
-# import distribution certificate to local keychain (required for XCode signing)
-IOS_DISTRIBUTION_CERT_PASSWORD="$IOS_DISTRIBUTION_CERT_PASSWORD" mobile/scripts/install-apple-certificate.sh
-# run cordova prepare and patch/amend necessary assets
-mobile/scripts/prepare-ios-app.sh
-# actually build, archive and export iOS application
-mobile/scripts/build-ios-app.sh
+# cd $TRAVIS_BUILD_DIR
+# # restore encrypted distribution certificate file
+# openssl aes-256-cbc -K $encrypted_d801e47d7db5_key -iv $encrypted_d801e47d7db5_iv -in mobile/ios-cert/distribution.p12.enc -out mobile/ios-cert/distribution.p12 -d
+# mobile/scripts/install-apple-profile.sh
+# # import distribution certificate to local keychain (required for XCode signing)
+# IOS_DISTRIBUTION_CERT_PASSWORD="$IOS_DISTRIBUTION_CERT_PASSWORD" mobile/scripts/install-apple-certificate.sh
+# # run cordova prepare and patch/amend necessary assets
+# mobile/scripts/prepare-ios-app.sh
+# # actually build, archive and export iOS application
+# mobile/scripts/build-ios-app.sh
