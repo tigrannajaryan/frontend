84917dd310ad2925e22a0c6f538e8e6180a5510c
Vladislav Shulyugin <vladislav.shulyugin@gmail.com>
2018-12-26 18:17:56 +0300

    Place pages back to app.module

--- mobile/stylist/src/app/app.module.ts
+++ mobile/stylist/src/app/app.module.ts
@@ -64,7 +64,45 @@ import { NotificationsApi } from './shared/push/notifications.api';
 import { MadeMenuComponent } from '~/core/components/made-menu/made-menu.component';
 import { CalendarPickerComponent } from '~/shared/components/calendar-picker/calendar-picker.component';
 
-import { pages } from '~/core/page-names';
+import { AboutComponent } from '~/about/about.component';
+import { AddServicesComponent } from '~/core/popups/add-services/add-services.component';
+import { AllClientsComponent } from '~/clients/all-clients/all-clients.component';
+import { AppointmentAddComponent } from '~/appointment/appointment-add/appointment-add';
+import { AppointmentCheckoutComponent } from '~/appointment/appointment-checkout/appointment-checkout.component';
+import { AppointmentServicesComponent } from '~/appointment/appointment-services/appointment-services';
+import { AuthConfirmPageComponent } from '~/auth/auth-confirm/auth-confirm.component';
+import { AuthPageComponent } from '~/auth/auth-start/auth-start.component';
+import { CalendarPrimingComponent } from '~/shared/components/calendar-priming/calendar-priming.component';
+import { ChangeGapTimeComponent } from '~/core/popups/change-gap-time/change-gap-time.component';
+import { ChangePercentComponent } from '~/core/popups/change-percent/change-percent.component';
+import { ClientDetailsComponent } from '~/clients/client-details/client-details.component';
+import { ClientsCalendarComponent } from '~/calendar/clients-calendar/clients-calendar.component';
+import { ConfirmCheckoutComponent } from '~/core/popups/confirm-checkout/confirm-checkout.component';
+import { DiscountsAlertComponent } from '~/discounts/discounts-alert/discounts-alert.component';
+import { DiscountsComponent } from '~/discounts/discounts.component';
+import { DiscountsFirstBookingComponent } from '~/discounts/discounts-first-booking/discounts-first-booking.component';
+import { DiscountsRevisitComponent } from '~/discounts/discounts-revisit/discounts-revisit.component';
+import { DiscountsWeekdayComponent } from '~/discounts/discounts-weekday/discounts-weekday.component';
+import { FirstScreenComponent } from '~/first-screen/first-screen';
+import { HomeSlotsComponent } from '~/home-slots/home-slots.component';
+import { InvitationsComponent } from '~/invitations/invitations.component';
+import { MyClientsComponent } from '~/clients/my-clients/my-clients.component';
+import { ProfileComponent } from '~/profile/profile.component';
+import { PushPrimingScreenComponent } from '~/shared/components/push-priming-screen/push-priming-screen.component';
+import { RegistrationDoneComponent } from '~/onboarding/registration-done/registration-done.component';
+import { ServiceItemComponent } from '~/services/services-item/services-item.component';
+import { ServicesCategoriesComponent } from '~/services/services-categories/services-categories.component';
+import { ServicesComponent } from '~/services/services.component';
+import { ServicesListComponent } from '~/services/services-list/services-list.component';
+import { WorkHoursComponent } from '~/workhours/workhours.component';
+
+// Onboarding
+import { CalendarExampleComponent } from '~/onboarding/calendar-example/calendar-example.component';
+import { ConnectInstagramComponent } from '~/onboarding/connect-instagram/connect-instagram.component';
+import { FieldEditComponent } from '~/onboarding/field-edit/field-edit.component';
+import { SalonAddressComponent } from '~/onboarding/salon-address/salon-address.component';
+import { StylistPhotoComponent } from '~/onboarding/stylist-photo/stylist-photo.component';
+import { WelcomeToMadeComponent } from '~/onboarding/welcome-to-made/welcome-to-made.component';
 
 initSentry();
 
@@ -103,8 +141,48 @@ if (!ENV.production) {
 }
 
 const declarations = [
-  ...pages,
+  // Pages
+  AboutComponent,
+  AddServicesComponent,
+  AllClientsComponent,
+  AppointmentAddComponent,
+  AppointmentCheckoutComponent,
+  AppointmentServicesComponent,
+  AuthConfirmPageComponent,
+  AuthPageComponent,
+  CalendarPrimingComponent,
+  ChangeGapTimeComponent,
+  ChangePercentComponent,
+  ClientDetailsComponent,
+  ClientsCalendarComponent,
+  ConfirmCheckoutComponent,
+  DiscountsAlertComponent,
+  DiscountsComponent,
+  DiscountsFirstBookingComponent,
+  DiscountsRevisitComponent,
+  DiscountsWeekdayComponent,
+  FirstScreenComponent,
+  HomeSlotsComponent,
+  InvitationsComponent,
+  MyClientsComponent,
+  ProfileComponent,
+  PushPrimingScreenComponent,
+  RegistrationDoneComponent,
+  ServiceItemComponent,
+  ServicesCategoriesComponent,
+  ServicesComponent,
+  ServicesListComponent,
+  WorkHoursComponent,
 
+  // Onboarding
+  CalendarExampleComponent,
+  ConnectInstagramComponent,
+  FieldEditComponent,
+  SalonAddressComponent,
+  StylistPhotoComponent,
+  WelcomeToMadeComponent,
+
+  // Components
   CalendarPickerComponent,
   MadeMenuComponent,
   MyAppComponent,
