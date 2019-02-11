d4d35a8a2ab1690a59a62a2d7e25155e36d81214
Alexei <alexeidjango@users.noreply.github.com>
2019-02-12 00:46:46 +0300

    [HOTFIX] Disable build-to-build plugin caching for Android builds (#1299)
    Android build seem to be unstable with build-to-build plugin folder
    persistency. For now we will only restore plugins build-to-build on
    osx builds.

--- mobile/scripts/restore-node-cache-plugins.sh
+++ mobile/scripts/restore-node-cache-plugins.sh
@@ -5,6 +5,8 @@ export AWS_SECRET_ACCESS_KEY="$AWS_SECURE_KEY"
 cd $TRAVIS_BUILD_DIR/mobile/$APP_TYPE
 sudo pip install awscli
 
+# only restore node on linux (i.e. android) builds. This persists between two
+# stages in the same build, no build-to-build persistency
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
     NODE_MODULES_ARCHIVE_FILENAME=$TRAVIS_BUILD_NUMBER-$APP_TYPE-$MB_ENV-node_modules.tar.gz
     echo "Restore node_modules from previous build phase"
@@ -12,7 +14,11 @@ if [[ $TRAVIS_OS_NAME == "linux" ]]; then
     tar -xzf $NODE_MODULES_ARCHIVE_FILENAME || true
 fi
 
-PLUGIN_ARCHIVE_FILENAME=$APP_TYPE-$MB_ENV-$TRAVIS_OS_NAME-plugins.tar.gz
-echo "Restore Cordova plugins cache"
-aws s3 cp s3://made-travis-cache/plugin-cache/$PLUGIN_ARCHIVE_FILENAME $TRAVIS_BUILD_DIR/mobile/$APP_TYPE/$PLUGIN_ARCHIVE_FILENAME || true
-tar -xzf $PLUGIN_ARCHIVE_FILENAME || true
+# Android seems to be unstable with build-to-build plugin folder persistency. For now
+# we will only restore plugins build-to-build on osx builds
+if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+    PLUGIN_ARCHIVE_FILENAME=$APP_TYPE-$MB_ENV-$TRAVIS_OS_NAME-plugins.tar.gz
+    echo "Restore Cordova plugins cache"
+    aws s3 cp s3://made-travis-cache/plugin-cache/$PLUGIN_ARCHIVE_FILENAME $TRAVIS_BUILD_DIR/mobile/$APP_TYPE/$PLUGIN_ARCHIVE_FILENAME || true
+    tar -xzf $PLUGIN_ARCHIVE_FILENAME || true
+fi
