17b9a737f6d7ea3d476efae228f31f2c05c4b5cd
Tigran Najaryan <tigran@najaryan.net>
2019-04-24 16:06:10 -0400

    Disable GA and Sentry
    Remove GA and Sentry ids from staging and production builds.

--- mobile/stylist/src/app/environments/environment.staging.ts
+++ mobile/stylist/src/app/environments/environment.staging.ts
@@ -6,9 +6,9 @@ export const ENV = {
   ffEnablePushNotifications: true,
 
   // Enable Sentry reporting on production
-  sentryDsn: 'https://b238c730d93f4e11917f5f716dc1920a@sentry.io/1229223',
+  sentryDsn: undefined,
 
-  gaTrackingId: 'UA-120898935-1',
+  gaTrackingId: undefined,
 
   // Firebase Cloud Messaging Sender ID
   FCM_PUSH_SENDER_ID: '17636556416',
